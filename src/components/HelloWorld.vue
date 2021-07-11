8fe83a412a3a9203ed5ea4f1ba9f98215adacf9c
umzo <umeda2444@gmail.com>
2021-07-11 23:14:57 +0900

    add vue.config.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg2 }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,8 +35,15 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      msg2: 'hoge',
+    }
   }
 }
+
+console.log("export HelloWorld.vue")
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
